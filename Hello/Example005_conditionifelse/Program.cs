be59b631e225e9f7a2dc216fd98657acd8769ade
Mikhail <moytorin@bk.ru>
2022-09-18 18:59:00 +0300

    add Example005_conditionifelse

---
+++ Hello/Example005_conditionifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Напиши имя этого Mazafaka-Nigger'a: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "Nigga")
+{
+    Console.WriteLine("Wassup, broo");
+}
+else
+{
+    Console.Write("Yo, ");
+    Console.WriteLine(username);
+}
